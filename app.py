38e6bc4b1a0b6f6b423e2a6eec0ef2c14515c5ec
Niklas Heer <niklas.heer@tradebyte.com>
2019-07-31 17:23:36 +0200

    :bug: Fix error when no new event is present. closes #1

--- app.py
+++ app.py
@@ -2,18 +2,24 @@ from http.server import BaseHTTPRequestHandler
 import meetup.api
 import debugserver
 
+
 def get_meetup_url():
     client = meetup.api.Client()
-    group_info = client.GetGroup({'urlname': 'dev_night'})
-    next_event = client.GetEvent(group_info.next_event)
-    return next_event.event_url
+    group_info = client.GetGroup({"urlname": "dev_night"})
+    url = "https://www.meetup.com/de-DE/dev_night/"
+    try:
+        next_event = client.GetEvent(group_info.next_event)
+        url = next_event.event_url
+    except:
+        print("[INFO] No new event redirecting to meetup page.")
+
+    return url
 
 
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header("Content-type", "text/html")
         self.end_headers()
 
         reponse = f"""
@@ -27,5 +33,6 @@ class handler(BaseHTTPRequestHandler):
 
         return
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     debugserver.serve(handler)
